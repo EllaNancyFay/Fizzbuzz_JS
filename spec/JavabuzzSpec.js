26ddccd0e8d0d7c3c965368bd4f437af7bbaf196
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-14 14:12:36 +0100

    Refactor spec file

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,20 +1,21 @@
 describe('Javabuzz', function() {
 var javabuzz;
 
+ beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
 describe('knows when a number is', function() {
 
 	it('divisible by three', function() {
-		javabuzz = new Javabuzz();
 		expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
 	});
 
 	it('divisible by five', function() {
-		javabuzz = new Javabuzz();
 		expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
 	});
 
 	it('divisible by fifteen', function () {
-		javabuzz = new Javabuzz();
 		expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
 	});
 
@@ -23,18 +24,15 @@ describe('knows when a number is', function() {
 describe('knows when a number is NOT', function() {
 
 	it('divisible by three', function() {
-		javabuzz = new Javabuzz();
 		expect(javabuzz.isDivisibleByThree(1)).toBe(false);
 	});
 
 	it('divisibile by five', function() {
-		javabuzz = new Javabuzz();
 		expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
 
 	});
 
 	it('divisible by fifteen', function() {
-		javabuzz = new Javabuzz();
 		expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
 	});
 });
