ea9eccca1040713b999cf5ec3a6a4419cc912d8a
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-14 12:09:42 +0100

    Knows when a number is divisible by 3, 5 and 15

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,31 @@
+describe('Javabuzz', function() {
+var javabuzz;
+
+describe('knows when a number is', function() {
+
+it('divisible by three', function() {
+	javabuzz = new Javabuzz();
+	expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+});
+
+it('divisible by five', function() {
+	javabuzz = new Javabuzz();
+	expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+});
+
+it('divisible by fifteen', function () {
+	javabuzz = new Javabuzz();
+	expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
+});
+
+});
+
+describe('knows when a number is NOT', function() {
+ it('divisible by three', function() {
+	javabuzz = new Javabuzz();
+	expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+});
+
+});
+
+});
